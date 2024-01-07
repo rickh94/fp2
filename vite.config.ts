c405085e640e3b9d43f7220e33007324601fbfc4
Rick Henry <rickhenry@rickhenry.dev>
2024-01-07 01:58:41 -0500

    switch to netlify
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,7 @@ import { defineConfig } from "@solidjs/start/config";
 export default defineConfig({
   start: {
     server: {
-      preset: "cloudflare_module",
-      rollupConfig: {
-        external: ["__STATIC_CONTENT_MANIFEST", "node:async_hooks"],
-      },
+      preset: "netlify",
     },
   },
 });
